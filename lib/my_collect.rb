edd5fb83517a0cbf9f99baf1db45083d9bc5c097
Samantha Jordan <shinyguns@gmail.com>
2020-01-28 01:42:35 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
 
-
+def my_collect(argument)
+  if block_given?
+    i = 0
+    collection = []
+    while i < argument.length
+      collection << yield(argument[i])
+      i += 1
+    end
+  else
+    puts "Hey! Give me a block!"
+    return collection
+  end
+  my_collect(argument) do |lang|
+    lang.upcase
+  end
+end
